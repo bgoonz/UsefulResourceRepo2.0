d934ec37bb7ecd5f3fa9674df9e9c7c1d844f514
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:20:44 +0000

    update

---
+++ GIT-USERS/ashishdotme/ashishdotme.github.io/precache-manifest.2b9b2bc1f4de0633b9c00c59ff043b64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    revision: "0a80e18380715eb2663a",
+    url: "/static/css/main.2f1e5f94.chunk.css",
+  },
+  {
+    revision: "0a80e18380715eb2663a",
+    url: "/static/js/main.0a80e183.chunk.js",
+  },
+  {
+    revision: "1015926caf23046f2ba3",
+    url: "/static/js/1.1015926c.chunk.js",
+  },
+  {
+    revision: "229c360febb4351a89df",
+    url: "/static/js/runtime~main.229c360f.js",
+  },
+  {
+    revision: "c8c261c16462a489ae419376c56015f8",
+    url: "/index.html",
+  },
+];
---
+++ GIT-USERS/ashishdotme/ashishdotme.github.io/static/js/main.0a80e183.chunk.js
@@ -0,0 +1,424 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    14: function (e, t, a) {},
+    16: function (e, t, a) {},
+    18: function (e, t, a) {
+      "use strict";
+      a.r(t);
+      var n = a(0),
+        s = a.n(n),
+        c = a(7),
+        r = a.n(c),
+        l = (a(14), a(1)),
+        i = a(2),
+        m = a(4),
+        o = a(3),
+        u = a(5),
+        h = (function (e) {
+          function t() {
+            return (
+              Object(l.a)(this, t),
+              Object(m.a)(this, Object(o.a)(t).apply(this, arguments))
+            );
+          }
+          return (
+            Object(u.a)(t, e),
+            Object(i.a)(t, [
+              {
+                key: "render",
+                value: function () {
+                  return s.a.createElement(
+                    "div",
+                    null,
+                    s.a.createElement(
+                      "footer",
+                      { className: "footer" },
+                      s.a.createElement(
+                        "div",
+                        { className: "container" },
+                        s.a.createElement(
+                          "div",
+                          { className: "content has-text-centered" },
+                          s.a.createElement(
+                            "p",
+                            null,
+                            s.a.createElement(
+                              "strong",
+                              null,
+                              "Bulma Templates"
+                            ),
+                            " by",
+                            " ",
+                            s.a.createElement(
+                              "a",
+                              { href: "https://github.com/dansup" },
+                              "Daniel Supernault"
+                            ),
+                            " and",
+                            " ",
+                            s.a.createElement(
+                              "a",
+                              {
+                                href: "https://github.com/dansup/bulma-templates/graphs/contributors",
+                              },
+                              "many"
+                            ),
+                            " ",
+                            "contributors. The source code is licensed",
+                            s.a.createElement(
+                              "a",
+                              {
+                                href: "http://opensource.org/licenses/mit-license.php",
+                              },
+                              "MIT"
+                            ),
+                            "."
+                          ),
+                          s.a.createElement(
+                            "p",
+                            null,
+                            s.a.createElement(
+                              "a",
+                              {
+                                className: "icon",
+                                href: "https://github.com/dansup/bulma-templates",
+                              },
+                              s.a.createElement(
+                                "svg",
+                                {
+                                  xmlns: "http://www.w3.org/2000/svg",
+                                  width: "16",
+                                  height: "16",
+                                  viewBox: "0 0 16 16",
+                                },
+                                s.a.createElement("path", {
+                                  d: "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z",
+                                })
+                              )
+                            )
+                          )
+                        )
+                      )
+                    )
+                  );
+                },
+              },
+            ]),
+            t
+          );
+        })(n.Component),
+        d = (function (e) {
+          function t() {
+            return (
+              Object(l.a)(this, t),
+              Object(m.a)(this, Object(o.a)(t).apply(this, arguments))
+            );
+          }
+          return (
+            Object(u.a)(t, e),
+            Object(i.a)(t, [
+              {
+                key: "render",
+                value: function () {
+                  return s.a.createElement(
+                    "div",
+                    null,
+                    s.a.createElement(
+                      "section",
+                      { className: "hero is-info is-bold" },
+                      s.a.createElement(
+                        "div",
+                        { className: "hero-body" },
+                        s.a.createElement(
+                          "div",
+                          { className: "container has-text-centered" },
+                          s.a.createElement(
+                            "h1",
+                            { className: "title" },
+                            "Free Bulma Templates"
+                          ),
+                          s.a.createElement(
+                            "h2",
+                            { className: "subtitle" },
+                            "Based on the ",
+                            s.a.createElement(
+                              "a",
+                              { href: "http://bulma.io" },
+                              "Bulma"
+                            ),
+                            " CSS Framework"
+                          ),
+                          s.a.createElement(
+                            "a",
+                            {
+                              className: "github-button",
+                              href: "https://github.com/dansup/bulma-templates",
+                              "data-size": "large",
+                              "aria-label":
+                                "View dansup/bulma-templates on GitHub",
+                            },
+                            "View on Github"
+                          ),
+                          s.a.createElement(
+                            "a",
+                            {
+                              className: "github-button",
+                              href: "https://github.com/dansup/bulma-templates",
+                              "data-icon": "octicon-star",
+                              "data-size": "large",
+                              "data-show-count": "true",
+                              "aria-label":
+                                "Star dansup/bulma-templates on GitHub",
+                            },
+                            "Star"
+                          ),
+                          s.a.createElement(
+                            "a",
+                            {
+                              className: "github-button",
+                              href: "https://github.com/dansup/bulma-templates/fork",
+                              "data-size": "large",
+                              "data-show-count": "true",
+                              "aria-label":
+                                "Fork dansup/bulma-templates on GitHub",
+                            },
+                            "Fork"
+                          )
+                        )
+                      )
+                    )
+                  );
+                },
+              },
+            ]),
+            t
+          );
+        })(n.Component),
+        p = [
+          {
+            id: "0",
+            name: "React demos",
+            desc: ["My collection of react demos"],
+            date: "2019-02",
+            github: "https://github.com/ashishdotme/react-demos",
+            weight: 2,
+            website: ["https://github.com/ashishdotme/react-demos"],
+            tags: ["reactjs", "javascript"],
+          },
+          {
+            id: "1",
+            name: "React demos",
+            desc: ["My collection of react demos"],
+            date: "2019-02",
+            github: "https://github.com/ashishdotme/react-demos",
+            weight: 2,
+            website: ["https://github.com/ashishdotme/react-demos"],
+            tags: ["reactjs", "javascript"],
+          },
+          {
+            id: "2",
+            name: "React demos",
+            desc: ["My collection of react demos"],
+            date: "2019-02",
+            github: "https://github.com/ashishdotme/react-demos",
+            weight: 2,
+            website: ["https://github.com/ashishdotme/react-demos"],
+            tags: ["reactjs", "javascript"],
+          },
+          {
+            id: "3",
+            name: "React demos",
+            desc: ["My collection of react demos"],
+            date: "2019-02",
+            github: "https://github.com/ashishdotme/react-demos",
+            weight: 2,
+            website: ["https://github.com/ashishdotme/react-demos"],
+            tags: ["reactjs", "javascript"],
+          },
+        ];
+      p.sort(function (e, t) {
+        var a = new Date().getFullYear(),
+          n = function (e) {
+            if (e.indexOf("present") > -1) return a - 2012 + 1;
+            for (var t = a; t > 2012; t--)
+              if (e.indexOf(t.toString().substring(3)) > -1) return t - 2012;
+          },
+          s = n(e.date) + (e.weight || 0),
+          c = n(t.date) + (t.weight || 0);
+        return s < c ? 1 : -1;
+      });
+      var b = p,
+        g = function (e) {
+          return s.a.createElement(
+            "div",
+            { className: "column is-3 desktop-only" },
+            s.a.createElement(
+              "div",
+              { className: "card" },
+              s.a.createElement(
+                "header",
+                { className: "card-header" },
+                s.a.createElement(
+                  "p",
+                  { className: "card-header-title" },
+                  s.a.createElement(
+                    "span",
+                    null,
+                    "Admin ",
+                    s.a.createElement("small", null, "(Light)")
+                  ),
+                  s.a.createElement(
+                    "span",
+                    { className: "is-pulled-right" },
+                    s.a.createElement(
+                      "a",
+                      {
+                        href: "https://github.com/jgthms/bulma/releases/tag/0.7.2",
+                      },
+                      s.a.createElement(
+                        "span",
+                        { className: "tag is-default is-primary" },
+                        "v0.7.2"
+                      )
+                    ),
+                    s.a.createElement(
+                      "span",
+                      { className: "tag is-default is-info" },
+                      s.a.createElement("i", {
+                        className: "fas fa-desktop",
+                        title: "Desktop support",
+                      })
+                    ),
+                    s.a.createElement(
+                      "span",
+                      { className: "tag is-default is-info" },
+                      s.a.createElement("i", {
+                        className: "fas fa-mobile-alt",
+                        title: "Mobile support",
+                      })
+                    )
+                  )
+                )
+              ),
+              s.a.createElement(
+                "div",
+                { className: "card-content" },
+                s.a.createElement(
+                  "figure",
+                  { className: "image" },
+                  s.a.createElement("img", {
+                    src: "images/admin.png",
+                    alt: "Admin template screenshot",
+                  })
+                )
+              ),
+              s.a.createElement(
+                "footer",
+                { className: "card-footer" },
+                s.a.createElement(
+                  "a",
+                  {
+                    href: "templates/admin.html",
+                    className: "card-footer-item",
+                  },
+                  "Preview"
+                ),
+                s.a.createElement(
+                  "a",
+                  {
+                    href: "https://github.com/dansup/bulma-templates/blob/master/templates/admin.html",
+                    className: "card-footer-item",
+                  },
+                  "Source Code"
+                )
+              )
+            )
+          );
+        },
+        f = (function (e) {
+          function t() {
+            return (
+              Object(l.a)(this, t),
+              Object(m.a)(this, Object(o.a)(t).apply(this, arguments))
+            );
+          }
+          return (
+            Object(u.a)(t, e),
+            Object(i.a)(t, [
+              {
+                key: "render",
+                value: function () {
+                  var e = b.map(function (e) {
+                    return s.a.createElement(g, null);
+                  });
+                  return (
+                    console.log(e),
+                    s.a.createElement(
+                      "div",
+                      null,
+                      s.a.createElement(
+                        "div",
+                        { className: "container content-wrapper" },
+                        s.a.createElement(
+                          "div",
+                          { className: "columns is-multiline" },
+                          e
+                        )
+                      )
+                    )
+                  );
+                },
+              },
+            ]),
+            t
+          );
+        })(n.Component),
+        E =
+          (a(16),
+          (function (e) {
+            function t() {
+              return (
+                Object(l.a)(this, t),
+                Object(m.a)(this, Object(o.a)(t).apply(this, arguments))
+              );
+            }
+            return (
+              Object(u.a)(t, e),
+              Object(i.a)(t, [
+                {
+                  key: "render",
+                  value: function () {
+                    return s.a.createElement(
+                      "div",
+                      { className: "App" },
+                      s.a.createElement(d, null),
+                      s.a.createElement(f, null),
+                      s.a.createElement(h, null)
+                    );
+                  },
+                },
+              ]),
+              t
+            );
+          })(n.Component));
+      Boolean(
+        "localhost" === window.location.hostname ||
+          "[::1]" === window.location.hostname ||
+          window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+          )
+      );
+      r.a.render(s.a.createElement(E, null), document.getElementById("root")),
+        "serviceWorker" in navigator &&
+          navigator.serviceWorker.ready.then(function (e) {
+            e.unregister();
+          });
+    },
+    8: function (e, t, a) {
+      e.exports = a(18);
+    },
+  },
+  [[8, 2, 1]],
+]);
+//# sourceMappingURL=main.0a80e183.chunk.js.map
