6de052a17f7980022f89c727e374ca8073a2f232
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:30:40 +0000

    update

---
+++ GIT-USERS/lydia/django-tutorial/firstsite/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
