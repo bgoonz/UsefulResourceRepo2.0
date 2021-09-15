e060e59e6e3be0cb367a92fa1a0ef2b346ffcf91
bgoonz <bryan.guner@gmail.com>
2021-09-15 14:47:55 +0000

    microsoft

---
+++ _REPO/MICROSOFT/diner-wp/wprestaurantapp_WP75/LocalizedStrings.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Copyright (c) 2011-2014 Microsoft Mobile. All rights reserved.
+ *
+ * Nokia, Nokia Connecting People, Nokia Developer, and HERE are trademarks
+ * and/or registered trademarks of Nokia Corporation. Other product and company
+ * names mentioned herein may be trademarks or trade names of their respective
+ * owners.
+ *
+ * See the license text file delivered with this project for more information.
+ */
+
+namespace wprestaurantapp
+{
+    /// <summary>
+    /// Helper class for accessing application resources from XAML, especially for localization
+    /// </summary>
+    public class LocalizedStrings
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LocalizedStrings()
+        {
+        }
+
+        /// <summary>
+        /// Class variable for instance of AppResources
+        /// </summary>
+        private static wprestaurantapp.AppResources localizedResources = new wprestaurantapp.AppResources();
+
+        /// <summary>
+        /// Property for accessing AppResources
+        /// </summary>
+        public wprestaurantapp.AppResources LocalizedResources { get { return localizedResources; } }
+
+    }
+}
