cb064c4e6a20cece7df721f2e7937a65521342cd
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:21:43 +0000

    update

---
+++ GIT-USERS/ashishdotme/node-websockets/server.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const express = require("express");
+const { Server } = require("ws");
+
+const PORT = process.env.PORT || 3000;
+const INDEX = "/index.html";
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(
+      JSON.stringify({ peopleCount: Math.floor(Math.random() * 11) })
+    );
+  });
+}, 10000);
