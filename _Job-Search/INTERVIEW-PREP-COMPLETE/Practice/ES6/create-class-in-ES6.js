fac40637de27b4dcdac49093093e35d97e6dc0cf
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:33:14 +0000

    update

---
+++ _Job-Search/INTERVIEW-PREP-COMPLETE/Practice/ES6/create-class-in-ES6.js
@@ -0,0 +1,13 @@
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getUserData() {
+    console.log(this.name + " is " + this.age + " years old.");
+  }
+}
+
+let user = new User("paul", 18);
+user.getUserData();
