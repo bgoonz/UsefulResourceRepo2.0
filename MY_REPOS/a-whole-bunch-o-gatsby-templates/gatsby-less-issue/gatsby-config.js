c91865a5fa23bd5d243c085ff9906b6635da8ed8
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:36:44 +0000

    update

---
+++ MY_REPOS/a-whole-bunch-o-gatsby-templates/gatsby-less-issue/gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-less`,
+        options: {
+          javascriptEnabled: true,
+      }
+    }
+  ]
+}
---
+++ MY_REPOS/a-whole-bunch-o-gatsby-templates/gatsby-less-issue/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./index.module.less"
+
+export default () => <div>
+  <h1>Hello World</h1>
+  <div className={styles.test01}>content of test 01</div>
+  <div className={styles.test02}>content of test 02</div>
+</div>
