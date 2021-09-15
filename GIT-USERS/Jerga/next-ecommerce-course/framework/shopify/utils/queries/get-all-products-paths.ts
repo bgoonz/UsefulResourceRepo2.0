c76d72160b42b1d732b55a559269531d9cebcdf3
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:25:15 +0000

    update

---
+++ GIT-USERS/Jerga/next-ecommerce-course/framework/shopify/utils/queries/get-all-products-paths.ts
@@ -0,0 +1,13 @@
+const getAllProductsPathsQuery = `
+  query getAllProductsPaths($first: Int = 250) {
+    products(first: $first) {
+      edges {
+        node {
+          handle
+        }
+      }
+    }
+  }
+`;
+
+export default getAllProductsPathsQuery;
