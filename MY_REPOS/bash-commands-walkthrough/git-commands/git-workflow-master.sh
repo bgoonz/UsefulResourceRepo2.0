7938823d46ea2572b668639e5b182b069381a931
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:41:54 +0000

    update

---
+++ MY_REPOS/bash-commands-walkthrough/git-commands/git-workflow-master.sh
@@ -0,0 +1,8 @@
+# to be run in the root directory of an existing git repository that has already pushed changes to it's remote counterpart at least once.
+
+
+git init
+git add .
+git commit -m"update"
+git push -u origin master
+
