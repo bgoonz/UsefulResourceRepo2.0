e19e9084abb6ad456ac77f503e8edbf6511d50a4
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:26:13 +0000

    update

---
+++ GIT-USERS/Jerga/react-basics-yt/src/App.js
@@ -0,0 +1,9 @@
+// function App() {
+//   return (
+//     <h1>My Custom Component</h1>
+//   )
+// }
+
+const App = () => <h1>My Custom Component from ./app.js</h1>;
+
+export default App;
---
+++ GIT-USERS/Jerga/react-basics-yt/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
