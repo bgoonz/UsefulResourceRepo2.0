b2c535481da6423ad474b3235ac98c1d10ed2d7c
bgoonz <bryan.guner@gmail.com>
2021-09-15 14:40:23 +0000

    hi

---
+++ _REPO/FCC/fcc/client/src/components/landing/Landing.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+import IndexPage from '../../pages';
+import mockChallengeNodes from '../../__mocks__/challenge-nodes';
+
+jest.mock('../../analytics');
+
+describe('<Landing />', () => {
+  it('renders when visiting index page and logged out', () => {
+    const shallow = new ShallowRenderer();
+    shallow.render(<IndexPage {...loggedOutProps} />);
+    const result = shallow.getRenderOutput();
+    expect(result.type.displayName === 'Landing').toBeTruthy();
+  });
+});
+
+const loggedOutProps = {
+  data: { allChallengeNode: { edges: mockChallengeNodes } },
+  fetchState: {
+    complete: true,
+    error: null,
+    errored: false,
+    pending: false
+  },
+  isSignedIn: false,
+  user: {}
+};
