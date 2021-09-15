30283e336805f2cc523dabccb97ae1fd1d6f49f1
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:26:14 +0000

    update

---
+++ GIT-USERS/Jerga/ReactBoilerplate/app/app.jsx
@@ -0,0 +1,19 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var { Route, Router, IndexRoute, hashHistory } = require("react-router");
+
+var Main = require("Main");
+
+//Load Foundation
+require("style!css!foundation-sites/dist/foundation.min.css");
+$(document).foundation();
+
+// App css
+require("style!css!sass!applicationStyles");
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}></Route>
+  </Router>,
+  document.getElementById("app")
+);
---
+++ GIT-USERS/Jerga/ReactBoilerplate/app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require("react");
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Main.jsx rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Main;
