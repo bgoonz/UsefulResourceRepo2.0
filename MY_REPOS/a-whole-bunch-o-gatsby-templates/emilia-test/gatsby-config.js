881ced1ee0237f4b5998f41fa3d3c64743d4d8c3
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:34:57 +0000

    update

---
+++ MY_REPOS/a-whole-bunch-o-gatsby-templates/emilia-test/gatsby-config.js
@@ -0,0 +1,14 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {},
+    },
+  ]
+}
