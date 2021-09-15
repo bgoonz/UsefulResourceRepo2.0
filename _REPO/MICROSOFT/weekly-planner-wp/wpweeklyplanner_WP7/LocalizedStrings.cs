13d91edf8672dcf39ce020035410d090ed56e26b
bgoonz <bryan.guner@gmail.com>
2021-09-15 14:51:22 +0000

    microsoft

---
+++ _REPO/MICROSOFT/weekly-planner-wp/wpweeklyplanner_WP7/LocalizedStrings.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Copyright (c) 2011-2014 Microsoft Mobile. All rights reserved.
+ *
+ * Nokia, Nokia Connecting People, Nokia Developer, and HERE are trademarks
+ * and/or registered trademarks of Nokia Corporation. Other product and company
+ * names mentioned herein may be trademarks or trade names of their respective
+ * owners.
+ *
+ * See the license text file delivered with this project for more information.
+ */
+
+namespace wpweeklyplanner
+{
+    /// <summary>
+    /// Helper class for accessing application resources from XAML, especially for localization
+    /// </summary>
+    public class LocalizedStrings
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LocalizedStrings()
+        {
+        }
+
+        /// <summary>
+        /// Class variable for instance of AppResources
+        /// </summary>
+        private static wpweeklyplanner.AppResources localizedResources = new wpweeklyplanner.AppResources();
+
+        /// <summary>
+        /// Property for accessing AppResources
+        /// </summary>
+        public wpweeklyplanner.AppResources LocalizedResources { get { return localizedResources; } }
+
+    }
+}
