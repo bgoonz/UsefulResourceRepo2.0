06f28827f87777720ecd3521ffd483e5d7ff3fe5
bgoonz <bryan.guner@gmail.com>
2021-09-15 15:47:56 +0000

    hi

---
+++ MY_REPOS/js-data-structures-refined/index.js
@@ -0,0 +1,58 @@
+// data structures
+const LinkedList = require("./data-structures/linked-lists/linked-list");
+const ListNode = require("./data-structures/linked-lists/node");
+const Queue = require("./data-structures/queues/queue");
+const Stack = require("./data-structures/stacks/stack");
+const Graph = require("./data-structures/graphs/graph");
+const HashMap = require("./data-structures/maps/hash-maps/hash-map");
+const TreeMap = require("./data-structures/maps/tree-maps/tree-map");
+const HashSet = require("./data-structures/sets/hash-set");
+const TreeSet = require("./data-structures/sets/tree-set");
+const ArraySet = require("./data-structures/sets/array-set");
+const MapSet = require("./data-structures/sets/map-set");
+const BinarySearchTree = require("./data-structures/trees/binary-search-tree");
+const BinaryTreeNode = require("./data-structures/trees/binary-tree-node");
+const AvlTree = require("./data-structures/trees/avl-tree");
+const RedBlackTree = require("./data-structures/trees/red-black-tree");
+const LRUCache = require("./data-structures/custom/lru-cache");
+const Trie = require("./data-structures/trees/trie");
+const Heap = require("./data-structures/heaps/heap");
+const MinHeap = require("./data-structures/heaps/min-heap");
+const MaxHeap = require("./data-structures/heaps/max-heap");
+const PriorityQueue = require("./data-structures/heaps/priority-queue");
+
+// algorithms
+const bubbleSort = require("./algorithms/sorting/bubble-sort");
+const insertionSort = require("./algorithms/sorting/insertion-sort");
+const selectionSort = require("./algorithms/sorting/selection-sort");
+const quickSort = require("./algorithms/sorting/quick-sort");
+const mergeSort = require("./algorithms/sorting/merge-sort");
+
+module.exports = {
+  LinkedList,
+  ListNode,
+  Queue,
+  Stack,
+  Graph,
+  HashMap,
+  TreeMap,
+  HashSet,
+  TreeSet,
+  ArraySet,
+  MapSet,
+  BinarySearchTree,
+  BinaryTreeNode,
+  AvlTree,
+  RedBlackTree,
+  LRUCache,
+  Trie,
+  Heap,
+  MinHeap,
+  MaxHeap,
+  PriorityQueue,
+  bubbleSort,
+  insertionSort,
+  selectionSort,
+  quickSort,
+  mergeSort,
+};
---
+++ MY_REPOS/js-data-structures-refined/runtimes/04-merge-sort.js
@@ -0,0 +1,61 @@
+const assert = require("assert");
+
+// tag::merge[]
+/**
+ * Merge two arrays in asc order
+ * @example
+ *    merge([2,5,9], [1,6,7]) => [1, 2, 5, 6, 7, 9]
+ * @param {array} a
+ * @param {array} b
+ * @returns {array} merged arrays in asc order
+ */
+function merge(a = [], b = []) {
+  const merged = [];
+  // merge elements on a and b in asc order. Run-time O(a + b)
+  for (let ai = 0, bi = 0; ai < a.length || bi < b.length; ) {
+    if (ai >= a.length || a[ai] > b[bi]) {
+      merged.push(b[bi++]);
+    } else {
+      merged.push(a[ai++]);
+    }
+  }
+  return merged;
+}
+// end::merge[]
+
+// tag::sort[]
+/**
+ * Sort array in asc order using merge-sort
+ * @example
+ *    sort([3, 2, 1]) => [1, 2, 3]
+ *    sort([3]) => [3]
+ *    sort([3, 2]) => [2, 3]
+ * @param {array} array
+ */
+function sort(array = []) {
+  const size = array.length;
+  // base case
+  if (size < 2) {
+    return array;
+  }
+
+  switch (size) {
+  case 2:
+    return array[0] > array[1] ? [array[1], array[0]] : array;
+  }
+
+  // slit and merge
+  const mid = parseInt(size / 2, 10);
+  return merge(sort(array.slice(0, mid)), sort(array.slice(mid)));
+}
+// end::sort[]
+
+assert.deepStrictEqual(sort([3, 2, 1]), [1, 2, 3]);
+assert.deepStrictEqual(sort([3]), [3]);
+assert.deepStrictEqual(sort([3, 2]), [2, 3]);
+assert.deepStrictEqual(sort([]), []);
+assert.deepStrictEqual(sort(), []);
+
+assert.deepStrictEqual(merge([2, 5, 9], [1, 6, 7]), [1, 2, 5, 6, 7, 9]);
+assert.deepStrictEqual(merge(), []);
+assert.deepStrictEqual(merge([3, 5, 7], [2, 4]), [2, 3, 4, 5, 7]);
