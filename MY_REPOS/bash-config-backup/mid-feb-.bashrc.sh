2b5da62cc14535e7e9e0c53b0d07a13c7cceab7c
bgoonz <bryan.guner@gmail.com>
2021-09-15 14:52:27 +0000

    update

---
+++ MY_REPOS/bash-config-backup/mid-feb-.bashrc.sh
@@ -0,0 +1,360 @@
+# Path to your oh-my-bash installation.
+export OSH=/home/bryan/.oh-my-bash
+
+# Set name of the theme to load. Optionally, if you set this to "random"
+# it'll load a random theme each time that oh-my-bash is loaded.
+OSH_THEME="hawaii50"
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion. Case
+# sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment the following line to change how often to auto-update (in days).
+# export UPDATE_OSH_DAYS=13
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $OSH/custom?
+# OSH_CUSTOM=/path/to/new-custom-folder
+
+# Which completions would you like to load? (completions can be found in ~/.oh-my-bash/completions/*)
+# Custom completions may be added to ~/.oh-my-bash/custom/completions/
+# Example format: completions=(ssh git bundler gem pip pip3)
+# Add wisely, as too many completions slow down shell startup.
+completions=(
+  git
+  composer
+  ssh
+)
+
+# Which aliases would you like to load? (aliases can be found in ~/.oh-my-bash/aliases/*)
+# Custom aliases may be added to ~/.oh-my-bash/custom/aliases/
+# Example format: aliases=(vagrant composer git-avh)
+# Add wisely, as too many aliases slow down shell startup.
+aliases=(
+  general
+)
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-bash/plugins/*)
+# Custom plugins may be added to ~/.oh-my-bash/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+  git
+  bashmarks
+  npm
+  nvm
+  progress
+  sdkman
+  
+)
+
+source $OSH/oh-my-bash.sh
+
+# User configuration
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+export SSH_KEY_PATH="~/.ssh/rsa_id"
+
+# Set personal aliases, overriding those provided by oh-my-bash libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-bash
+# users are encouraged to define aliases within the OSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# ------------------------------------------------------------------------------
+# ---------------------------- general shortcuts--------------------------------
+# ------------------------------------------------------------------------------
+
+# -- easy reverse cd --
+
+alias .1='cd ..'
+alias .2='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+
+# -- easy ls with color--
+
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+
+#order by size
+alias lt='ls --human-readable --size -1 -S --classify'
+
+#only show hidden files
+alias lh='ls -lisAd .[^.]*'
+
+# -- grep with color --
+
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+
+# -- safety nets --
+
+alias mv='mv -i'
+alias rm='rm -i'
+
+# -- system checks --
+
+alias meminfo='free -m -l -t'
+alias topmem='ps auxf | sort -nr -k 4'
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
+alias  getipe='curl ipinfo.io/ip'
+alias speed='speedtest-cli --server 2406 --simple'
+
+# -- true laziness --
+
+alias c='clear'
+alias update='sudo apt update && sudo apt upgrade'
+alias install='sudo apt install'
+alias change-terminal='sudo update-alternatives --config x-terminal-emulator'
+alias pip="pip3"
+alias www='python -m SimpleHTTPServer 8000'
+alias vim='nvim'
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------ git shortcuts----------------------------------
+# ------------------------------------------------------------------------------
+
+alias commit='git commit -m'
+alias gc='git checkout'
+alias log='git log --oneline'
+alias status='git status'
+alias setremote='git remote set-url origin'
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------ psql shortcuts---------------------------------
+# ------------------------------------------------------------------------------
+
+alias psql='psql -h localhost'
+alias sc='npx sequelize-cli'
+alias sc-init='npx sequelize-cli init'
+
+alias sc-makedb='npx sequelize-cli db:create'
+alias sc-makemodel='npx sequelize-cli model:generate'
+
+alias sc-migrate='npx sequelize-cli db:migrate'
+
+alias sc-genseed='npx sequelize-cli seed:generate'
+alias sc-seed='npm sequelize-cli db:seed:all'
+###-begin-npm-completion-###
+#
+# npm command completion script
+#
+# Installation: npm completion >> ~/.bashrc  (or ~/.zshrc)
+# Or, maybe: npm completion > /usr/local/etc/bash_completion.d/npm
+#
+
+
+
+##------------------------------------
+
+alias ls='ls --color=auto'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias back='cd -'
+
+alias l='ls'
+alias sl='ls'
+
+alias ini="git init && git add . && git commit -m 'initial commit'"
+alias norm="git add . && git commit -m 'update'"
+alias commit='git commit'
+alias check='git checkout'
+alias status='git status'
+alias push='git push'
+alias pull='git pull'
+alias stash='git stash'
+alias pop='git stash pop'
+
+
+
+alias unzipa='find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;'
+
+
+alias run='./manage.py runserver 0.0.0.0:8000'
+alias syncdb='./manage.py syncdb'
+alias numFiles='echo $(ls -1 | wc -l)'       # numFiles:     Count of non-hidden files in current dir
+alias make1mb='truncate -s 1m ./1MB.dat'     # make1mb:      Creates a file of 1mb size (all zeros)
+alias make5mb='truncate -s 5m ./5MB.dat'     # make5mb:      Creates a file of 5mb size (all zeros)
+alias make10mb='truncate -s 10m ./10MB.dat'  # make10mb:     Creates a file of 10mb size (all zeros)
+
+
+#   ---------------------------
+#   2.  SEARCHING
+#   ---------------------------
+
+alias qfind="find . -name "                 # qfind:    Quickly search for file
+
+
+#   ---------------------------
+#   3.  PROCESS MANAGEMENT
+#   ---------------------------
+
+#   memHogsTop, memHogsPs:  Find memory hogs
+#   -----------------------------------------------------
+    alias memHogsTop='top -l 1 -o rsize | head -20'
+    alias memHogsPs='ps wwaxm -o pid,stat,vsize,rss,time,command | head -10'
+
+#   cpuHogs:  Find CPU hogs
+#   -----------------------------------------------------
+    alias cpu_hogs='ps wwaxr -o pid,stat,%cpu,time,command | head -10'
+
+#   topForever:  Continual 'top' listing (every 10 seconds)
+#   -----------------------------------------------------
+    alias topForever='top -l 9999999 -s 10 -o cpu'
+
+#   ttop:  Recommended 'top' invocation to minimize resources
+#   ------------------------------------------------------------
+#       Taken from this macosxhints article
+#       http://www.macosxhints.com/article.php?story=20060816123853639
+#   ------------------------------------------------------------
+    alias ttop="top -R -F -s 10 -o rsize"
+
+
+#   ---------------------------
+#   4.  NETWORKING
+#   ---------------------------
+
+alias netCons='lsof -i'                                   # netCons:      Show all open TCP/IP sockets
+alias lsock='sudo /usr/sbin/lsof -i -P'                   # lsock:        Display open sockets
+alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP'         # lsockU:       Display only open UDP sockets
+alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'         # lsockT:       Display only open TCP sockets
+alias ipInfo0='ifconfig getpacket en0'                    # ipInfo0:      Get info on connections for en0
+alias ipInfo1='ifconfig getpacket en1'                    # ipInfo1:      Get info on connections for en1
+alias openPorts='sudo lsof -i | grep LISTEN'              # openPorts:    All listening connections
+alias showBlocked='sudo ipfw list'                        # showBlocked:  All ipfw rules inc/ blocked IPs
+
+
+#   ---------------------------------------
+#   5.  SYSTEMS OPERATIONS & INFORMATION
+#   ---------------------------------------
+
+alias mountReadWrite='/sbin/mount -uw /'    # mountReadWrite:   For use when booted into single-user
+
+
+#   ---------------------------------------
+#   6.  DATE & TIME MANAGEMENT
+#   ---------------------------------------
+
+alias bdate="date '+%a, %b %d %Y %T %Z'"
+alias cal3='cal -3'
+alias da='date "+%Y-%m-%d %A    %T %Z"'
+alias daysleft='echo "There are $(($(date +%j -d"Dec 31, $(date +%Y)")-$(date +%j))) left in year $(date +%Y)."'
+alias epochtime='date +%s'
+alias mytime='date +%H:%M:%S'
+alias secconvert='date -d@1234567890'
+alias stamp='date "+%Y%m%d%a%H%M"'
+alias timestamp='date "+%Y%m%dT%H%M%S"'
+alias today='date +"%A, %B %-d, %Y"'
+alias weeknum='date +%V'
+# Example aliases
+# alias bashconfig="mate ~/.bashrc"
+# alias ohmybash="mate ~/.oh-my-bash"
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# ------------------------------------------------------------------------------
+# ---- controls what displays on every line of terminal that is not the output log-----------
+#--- the \W outputs the current working directory
+# ------------------------------------------------------------------------------
+
+#--------------------function that ls's every time I cd into a new directory-----------
+#   i.e.)       home ====>cd ..
+#bin   dev  home  lib    lib64   lost+found  mnt  proc  run   snap  sys  usr
+#boot  etc  init  lib32  libx32  media       opt  root  sbin  srv   tmp  var
+#/ ====>
+#------------------------------------------------------------------------------------------------
+function cd {
+    if [ -z "$1" ]; then
+        builtin cd
+    else
+        builtin cd "$1"
+    fi
+    if [ $? -eq 0 ]; then
+    printf '\n'
+        ls
+    fi
+}
+#------------------------------------------------------------------------------------------------
+
+
+
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
