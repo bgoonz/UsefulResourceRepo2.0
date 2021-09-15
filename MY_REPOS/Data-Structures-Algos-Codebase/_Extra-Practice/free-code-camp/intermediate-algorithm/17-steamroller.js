65b77acfa98ae9edccd0dd4b4ce02de14ad2f69c
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:46:32 +0000

    update

---
+++ MY_REPOS/Data-Structures-Algos-Codebase/_Extra-Practice/free-code-camp/intermediate-algorithm/17-steamroller.js
@@ -0,0 +1,13 @@
+function steamrollArray( arr ) {
+  if ( Array.isArray( arr ) ) {
+    return arr.reduce( ( done, curr ) => done.concat( steamrollArray( curr ) ), [] );
+  } else {
+    return arr;
+  }
+}
+
+console.log( steamrollArray( [ 1, [ 2 ],
+  [ 3, [
+    [ 4 ]
+  ] ]
+] ) );
