9754bcafffe9c9b96817dc68fcd01c5006bfd89c
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:24:04 +0000

    update

---
+++ GIT-USERS/Jerga/electron-react-typescript-boilerplate/preload.js
@@ -0,0 +1,11 @@
+const { ipcRenderer, contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("electron", {
+  notificationApi: {
+    sendNotification(message) {
+      ipcRenderer.send("notify", message);
+    },
+  },
+  batteryApi: {},
+  filesApi: {},
+});
