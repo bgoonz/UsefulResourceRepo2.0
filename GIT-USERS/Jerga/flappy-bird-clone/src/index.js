c6ea9259ea16c14a8b716b482270ca26e49d77a4
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:24:12 +0000

    update

---
+++ GIT-USERS/Jerga/flappy-bird-clone/src/index.js
@@ -0,0 +1,36 @@
+import Phaser from "phaser";
+
+import PlayScene from "./scenes/PlayScene";
+import MenuScene from "./scenes/MenuScene";
+import PreloadScene from "./scenes/PreloadScene";
+import ScoreScene from "./scenes/ScoreScene";
+import PauseScene from "./scenes/PauseScene";
+
+const WIDTH = 400;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION,
+};
+
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
+const createScene = (Scene) => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
+
+const config = {
+  type: Phaser.AUTO,
+  ...SHARED_CONFIG,
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      // debug: true,
+    },
+  },
+  scene: initScenes(),
+};
+
+new Phaser.Game(config);
