24af39821f361fa2b7d5cd9726d5eb1902cb1c07
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:27:59 +0000

    update

---
+++ GIT-USERS/Jerga/vue-meetup-starter/server/controllers/posts.js
@@ -0,0 +1,15 @@
+const Thread = require("../models/threads");
+
+exports.getPosts = function (req, res) {
+  const threadId = req.query.threadId;
+
+  Post.find({ thread: threadId })
+    .populate("user")
+    .exec((errors, posts) => {
+      if (errors) {
+        return res.status(422).send({ errors });
+      }
+
+      return res.json(posts);
+    });
+};
---
+++ GIT-USERS/Jerga/vue-meetup-starter/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <TheNavbar />
+    <PageHome />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import PageHome from "@/pages/PageHome";
+import TheNavbar from "@/components/shared/TheNavbar";
+import TheFooter from "@/components/shared/TheFooter";
+export default {
+  name: "app",
+  components: {
+    PageHome,
+    TheNavbar,
+    TheFooter,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "assets/css/spacing.css";
+@import "~bulma/bulma.sass";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.bold {
+  font-weight: bold;
+}
+.cover {
+  background-size: cover;
+  /*background-position: center;*/
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+.hero {
+  position: relative;
+}
+.hero-body {
+  padding: 3rem 1.5rem;
+}
+.hero-bg {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("https://images.unsplash.com/photo-1531263060782-b024de9b9793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+</style>
