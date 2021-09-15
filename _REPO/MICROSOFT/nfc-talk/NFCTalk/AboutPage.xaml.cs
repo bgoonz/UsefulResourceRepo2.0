e1cc0385ebbd89854420712d6c4eb6709dc771e6
bgoonz <bryan.guner@gmail.com>
2021-09-15 14:49:16 +0000

    microsoft

---
+++ _REPO/MICROSOFT/nfc-talk/NFCTalk/AboutPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Copyright © 2012-2013-2014 Microsoft Mobile. All rights reserved.
+ * Nokia and Nokia Connecting People are registered trademarks of Nokia Corporation. 
+ * Other product and company names mentioned herein may be trademarks
+ * or trade names of their respective owners. 
+ * See LICENSE.TXT for license information.
+ */
+
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using System;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Xml.Linq;
+
+namespace NFCTalk
+{
+    public partial class AboutPage : PhoneApplicationPage
+    {
+        private NFCTalk.DataContext _dataContext = NFCTalk.DataContext.Singleton;
+        private ProgressIndicator _progressIndicator = new ProgressIndicator();
+
+        public AboutPage()
+        {
+            InitializeComponent();
+
+            _progressIndicator.IsIndeterminate = true;
+
+            versionTextBox.Text = String.Format("Version {0}", XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            _dataContext.Communication.Connected += Connected;
+            _dataContext.Communication.Connecting += Connecting;
+            _dataContext.Communication.ConnectivityProblem += ConnectivityProblem;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+
+            _dataContext.Communication.Connected -= Connected;
+            _dataContext.Communication.Connecting -= Connecting;
+            _dataContext.Communication.ConnectivityProblem -= ConnectivityProblem;
+        }
+
+        /// <summary>
+        /// Event handler to execute when connection is being made.
+        /// </summary>
+        private void Connecting()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                ShowProgress("Connecting...");
+            });
+        }
+
+        /// <summary>
+        /// Event handler to execute when connection has been established.
+        /// 
+        /// Application is navigated to TalkPage.
+        /// </summary>
+        private void Connected()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                HideProgress();
+
+                NavigationService.Navigate(new Uri("/TalkPage.xaml", UriKind.Relative));
+            });
+        }
+
+        /// <summary>
+        /// Event handler to execute when attempting to connect fails.
+        /// </summary>
+        private void ConnectivityProblem()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                HideProgress();
+            });
+        }
+
+        /// <summary>
+        /// Show system tray progress indicator.
+        /// </summary>
+        /// <param name="msg">Text to show</param>
+        private void ShowProgress(String msg)
+        {
+            _progressIndicator.Text = msg;
+            _progressIndicator.IsVisible = true;
+
+            SystemTray.SetProgressIndicator(this, _progressIndicator);
+        }
+
+        /// <summary>
+        /// Hide system tray progress indicator.
+        /// </summary>
+        private void HideProgress()
+        {
+            _progressIndicator.IsVisible = false;
+
+            SystemTray.SetProgressIndicator(this, _progressIndicator);
+        }
+    }
+}
---
+++ _REPO/MICROSOFT/nfc-talk/NFCTalk/PeersPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Copyright © 2013-2014 Microsoft Mobile. All rights reserved.
+ * Nokia and Nokia Connecting People are registered trademarks of Nokia Corporation. 
+ * Other product and company names mentioned herein may be trademarks
+ * or trade names of their respective owners. 
+ * See LICENSE.TXT for license information.
+ */
+
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Windows.Networking.Proximity;
+
+namespace NFCTalk
+{
+    public partial class PeersPage : PhoneApplicationPage
+    {
+        private NFCTalk.DataContext _dataContext = NFCTalk.DataContext.Singleton;
+        private ProgressIndicator _progressIndicator = new ProgressIndicator();
+
+        public PeersPage()
+        {
+            InitializeComponent();
+
+            DataContext = _dataContext;
+
+            _progressIndicator.IsIndeterminate = true;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            _dataContext.Communication.Connected += Connected;
+            _dataContext.Communication.Connecting += Connecting;
+            _dataContext.Communication.ConnectivityProblem += ConnectivityProblem;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+
+            _dataContext.Communication.Connected -= Connected;
+            _dataContext.Communication.Connecting -= Connecting;
+            _dataContext.Communication.ConnectivityProblem -= ConnectivityProblem;
+        }
+
+        /// <summary>
+        /// Event handler to execute when connection is being made.
+        /// </summary>
+        private void Connecting()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                PeersListBox.IsEnabled = false;
+
+                ShowProgress("Connecting...");
+            });
+        }
+
+        /// <summary>
+        /// Event handler to execute when connection has been established.
+        /// 
+        /// Application is navigated to TalkPage.
+        /// </summary>
+        private void Connected()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                HideProgress();
+
+                NavigationService.Navigate(new Uri("/TalkPage.xaml", UriKind.Relative));
+                NavigationService.RemoveBackEntry();
+            });
+        }
+
+        /// <summary>
+        /// Event handler to execute when attempting to connect fails.
+        /// </summary>
+        private void ConnectivityProblem()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                HideProgress();
+
+                NavigationService.GoBack();
+            });
+        }
+
+        /// <summary>
+        /// Show system tray progress indicator.
+        /// </summary>
+        /// <param name="msg">Text to show</param>
+        private void ShowProgress(String msg)
+        {
+            _progressIndicator.Text = msg;
+            _progressIndicator.IsVisible = true;
+
+            SystemTray.SetProgressIndicator(this, _progressIndicator);
+        }
+
+        /// <summary>
+        /// Hide system tray progress indicator.
+        /// </summary>
+        private void HideProgress()
+        {
+            _progressIndicator.IsVisible = false;
+
+            SystemTray.SetProgressIndicator(this, _progressIndicator);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PeerInformation peerInformation = PeersListBox.SelectedItem as PeerInformation;
+
+            if (peerInformation != null)
+            {
+                _dataContext.Communication.Connect(peerInformation);
+                //_dataContext.PeerInformation = peerInformation;
+            }
+        }
+    }
+}
