f9b77cd11a2290aa32fc1ddff0605a78dcf6b9ac
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:19:42 +0000

    update

---
+++ GIT-USERS/amitness/DeepMoji/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="deepmoji",
+    version="1.0",
+    packages=["deepmoji"],
+    description="DeepMoji library",
+    include_package_data=True,
+    install_requires=[
+        "emoji==0.4.5",
+        "h5py==2.8.0",
+        "Keras==2.0.9",
+        "numpy",
+        "scikit-learn",
+        "text-unidecode",
+    ],
+)
