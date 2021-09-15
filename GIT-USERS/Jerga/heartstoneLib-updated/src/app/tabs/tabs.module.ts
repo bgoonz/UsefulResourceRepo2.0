c8fcc94bef03997b0114cc421df2c9d8fc143528
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:24:34 +0000

    update

---
+++ GIT-USERS/Jerga/heartstoneLib-updated/src/app/tabs/tabs.module.ts
@@ -0,0 +1,22 @@
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { TabsPageRoutingModule } from "./tabs.router.module";
+
+import { TabsPage } from "./tabs.page";
+import { CardPageModule } from "../card/card.module";
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule,
+    CardPageModule,
+  ],
+  declarations: [TabsPage],
+})
+export class TabsPageModule {}
