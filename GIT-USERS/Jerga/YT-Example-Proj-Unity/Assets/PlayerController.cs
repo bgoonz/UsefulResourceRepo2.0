c2e5fb578dd10d6245af7970e5aaea35ee0671e1
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:28:09 +0000

    update

---
+++ GIT-USERS/Jerga/YT-Example-Proj-Unity/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_Speed;
+    public Camera followCamera;
+
+    private Rigidbody m_Rb;
+    private Vector3 m_CameraPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+        m_CameraPos = followCamera.transform.position - transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 playerPos = m_Rb.position;
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        if (movement == Vector3.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(movement);
+
+        targetRotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRotation,
+            360 * Time.fixedDeltaTime);
+
+        m_Rb.MovePosition(playerPos + movement * 10 * Time.fixedDeltaTime);
+        m_Rb.MoveRotation(targetRotation);
+    }
+
+    private void LateUpdate()
+    {
+        followCamera.transform.position = m_Rb.position + m_CameraPos;
+    }
+}
