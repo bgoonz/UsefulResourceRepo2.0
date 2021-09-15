7cb0977fb7e17671667a74a9aa996121015a75d7
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:23:05 +0000

    update

---
+++ GIT-USERS/Jerga/bwm-firebase-auth/src/app/rental/rental.module.ts
@@ -0,0 +1,84 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { NgPipesModule, UcWordsPipe } from "ngx-pipes";
+import { MapModule } from "../common/map/map.module";
+import { Daterangepicker } from "ng2-daterangepicker";
+import { FormsModule } from "@angular/forms";
+import { EditableModule } from "../common/components/editable/editable.module";
+import { ImageUploadModule } from "../common/components/image-upload/image-upload.module";
+import { PaymentModule } from "../payment/payment.module";
+
+import { RentalListComponent } from "./rental-list/rental-list.component";
+import { RentalListItemComponent } from "./rental-list-item/rental-list-item.component";
+import { RentalComponent } from "./rental.component";
+import { RentalDetailComponent } from "./rental-detail/rental-detail.component";
+import { RentalDetailBookingComponent } from "./rental-detail/rental-detail-booking/rental-detail-booking.component";
+import { RentalSearchComponent } from "./rental-search/rental-search.component";
+
+import { RentalService } from "./shared/rental.service";
+import { BookingService } from "../booking/shared/booking.service";
+import { HelperService } from "../common/service/helper.service";
+import { UppercasePipe } from "../common/pipes/uppercase.pipe";
+
+import { AuthGuard } from "../auth/shared/auth.guard";
+import { RentalGuard } from "./shared/rental.guard";
+import { RentalCreateComponent } from "./rental-create/rental-create.component";
+import { RentalUpdateComponent } from "./rental-update/rental-update.component";
+
+const routes: Routes = [
+  {
+    path: "rentals",
+    component: RentalComponent,
+    children: [
+      { path: "", component: RentalListComponent },
+      {
+        path: "new",
+        component: RentalCreateComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: ":rentalId/edit",
+        component: RentalUpdateComponent,
+        canActivate: [AuthGuard, RentalGuard],
+      },
+      { path: ":rentalId", component: RentalDetailComponent },
+      { path: ":city/homes", component: RentalSearchComponent },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [
+    RentalListComponent,
+    RentalListItemComponent,
+    RentalComponent,
+    RentalDetailComponent,
+    UppercasePipe,
+    RentalDetailBookingComponent,
+    RentalSearchComponent,
+    RentalCreateComponent,
+    RentalUpdateComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    NgPipesModule,
+    MapModule,
+    Daterangepicker,
+    FormsModule,
+    EditableModule,
+    ImageUploadModule,
+    PaymentModule,
+  ],
+  providers: [
+    RentalService,
+    HelperService,
+    BookingService,
+    UcWordsPipe,
+    RentalGuard,
+  ],
+})
+export class RentalModule {}
---
+++ GIT-USERS/Jerga/bwm-firebase-auth/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  STRIPE_PK: "pk_test_HgtMD9wHltUMpDw2mK4VbF6l",
+  firebase: {
+    apiKey: "AIzaSyAWscZr7gTB9HRAZpbKOo1-EvcMBCyXp0U",
+    authDomain: "vue-school-b9938.firebaseapp.com",
+    databaseURL: "https://vue-school-b9938.firebaseio.com",
+    projectId: "vue-school-b9938",
+    storageBucket: "vue-school-b9938.appspot.com",
+    messagingSenderId: "853695242693",
+  },
+};
