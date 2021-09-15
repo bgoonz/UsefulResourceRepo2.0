4f99db3e3365c75ab2abbca8c13f4a2de8cb9ae3
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:33:31 +0000

    update

---
+++ MY_REPOS/03-fetch-data/src/index.js
@@ -0,0 +1,33 @@
+import { LitElement, html } from "lit-element";
+
+class FetchingData extends LitElement {
+  static get properties() {
+    return {
+      response: { type: Array },
+    };
+  }
+
+  constructor() {
+    super();
+    this.response = [];
+  }
+
+  firstUpdated() {
+    fetch("https://swapi.dev/api/people/")
+      .then((r) => r.json())
+      .then((r) => {
+        this.response = r.results;
+      });
+  }
+
+  render() {
+    const { response } = this;
+    return html`
+      <ul>
+        ${response.map((item) => html` <li>${item.name}</li> `)}
+      </ul>
+    `;
+  }
+}
+
+customElements.define("fetching-data", FetchingData);
