33e73ee2cf96744cb55d076de18cf84a1dc09af2
bgoonz <bryan.guner@gmail.com>
2021-09-15 08:25:21 +0000

    update

---
+++ GIT-USERS/Jerga/nextjs-portfolio-template/pages/_app.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/index.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
