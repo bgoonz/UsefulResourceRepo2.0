70e1deb75878b6374a78770626a8d1497ed00f49
bgoonz <bryan.guner@gmail.com>
2021-09-22 04:19:47 +0000

    hi

---
+++ GIT-USERS/Jerga/portfolio-jerga-setup/components/BasePage.js
@@ -0,0 +1,28 @@
+import { Container } from "reactstrap";
+import PropTypes from "prop-types";
+
+const BasePage = (props) => {
+  const { className, title, containerClass } = props;
+
+  // const className = props.className || '';
+
+  return (
+    <div className={`base-page ${className}`}>
+      <Container className={containerClass}>
+        {title && (
+          <div className="page-header">
+            <h1 className="page-header-title">{title}</h1>
+          </div>
+        )}
+        {props.children}
+      </Container>
+    </div>
+  );
+};
+
+BasePage.defaultProps = {
+  className: "",
+  containerClass: "",
+};
+
+export default BasePage;
---
+++ GIT-USERS/Jerga/portfolio-jerga-setup/server/index.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+const next = require("next");
+const mongoose = require("mongoose");
+const routes = require("../routes");
+
+// SERVICE
+const authService = require("./services/auth");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = routes.getRequestHandler(app);
+const config = require("./config");
+
+const Book = require("./models/book");
+const bodyParser = require("body-parser");
+
+const bookRoutes = require("./routes/book");
+const portfolioRoutes = require("./routes/portfolio");
+const blogRoutes = require("./routes/blog");
+
+const robotsOptions = {
+  root: path.join(__dirname, "../static"),
+  headers: {
+    "Content-Type": "text/plain;charset=UTF-8",
+  },
+};
+
+const secretData = [
+  {
+    title: "SecretData 1",
+    description: "Plans how to build spaceship",
+  },
+  {
+    title: "SecretData 2",
+    description: "My secret passwords",
+  },
+];
+
+mongoose
+  .connect(config.DB_URI, { useNewUrlParser: true })
+  .then(() => console.log("Database Connected!"))
+  .catch((err) => console.error(err));
+
+// async () => (await mongoose.connect(config.DB_URI, { useNewUrlParser: true}))();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.use(compression());
+    server.use(bodyParser.json());
+
+    server.use("/api/v1/books", bookRoutes);
+    server.use("/api/v1/portfolios", portfolioRoutes);
+    server.use("/api/v1/blogs", blogRoutes);
+
+    server.get("/robots.txt", (req, res) => {
+      return res.status(200).sendFile("robots.txt", robotsOptions);
+    });
+
+    server.get("/api/v1/secret", authService.checkJWT, (req, res) => {
+      return res.json(secretData);
+    });
+
+    server.get(
+      "/api/v1/onlysiteowner",
+      authService.checkJWT,
+      authService.checkRole("siteOwner"),
+      (req, res) => {
+        return res.json(secretData);
+      }
+    );
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.use(function (err, req, res, next) {
+      if (err.name === "UnauthorizedError") {
+        res
+          .status(401)
+          .send({ title: "Unauthorized", detail: "Unauthorized Access!" });
+      }
+    });
+
+    const PORT = process.env.PORT || 3000;
+
+    server.use(handle).listen(PORT, (err) => {
+      if (err) throw err;
+      console.log("> Ready on port " + PORT);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
