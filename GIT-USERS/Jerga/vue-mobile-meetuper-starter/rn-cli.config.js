61322a32cb71bcdb0e7583aa0bdf053b705253e2
bgoonz <bryan.guner@gmail.com>
2021-09-22 04:31:21 +0000

    hi

---
+++ GIT-USERS/Jerga/vue-mobile-meetuper-starter/rn-cli.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("./vueTransformerPlugin.js"),
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "vue"],
+    },
+  };
+})();
