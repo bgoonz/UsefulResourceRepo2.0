d5422deba971609eda5ee9cf1cc715dea0702e89
bgoonz <bryan.guner@gmail.com>
2021-09-22 04:01:52 +0000

    hi

---
+++ _ORGS/FACEBOOK/docusaurus/babel.config.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+};
---
+++ _ORGS/FACEBOOK/docusaurus/packages/docusaurus/src/client/normalizeLocation.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {Location} from '@docusaurus/history';
+
+// Memoize previously normalized pathnames.
+const pathnames: Record<string, string> = {};
+
+function normalizeLocation<T extends Location>(location: T): T {
+  if (pathnames[location.pathname]) {
+    return {
+      ...location,
+      pathname: pathnames[location.pathname],
+    };
+  }
+
+  let pathname = location.pathname || '/';
+  pathname = pathname.trim().replace(/\/index\.html$/, '');
+
+  if (pathname === '') {
+    pathname = '/';
+  }
+
+  pathnames[location.pathname] = pathname;
+
+  return {
+    ...location,
+    pathname,
+  };
+}
+
+export default normalizeLocation;
