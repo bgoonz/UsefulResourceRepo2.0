896616986728d93faf680c80c9dca63e98c0d37b
bgoonz <bryan.guner@gmail.com>
2021-09-22 04:18:26 +0000

    hi

---
+++ GIT-USERS/Jerga/ionic-deploy/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var path = require("path");
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: "true" }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const appPath = path.join(__dirname, "platforms", "browser", "www");
+app.use(express.static(appPath));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(appPath, "index.html"));
+});
+
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
+});
---
+++ GIT-USERS/Jerga/ionic-deploy/src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { TabsPage } from "./tabs.page";
+import { CardDeckPage } from "../card/card-deck/card-deck.page";
+import { CardListingPage } from "../card/card-listing/card-listing.page";
+import { CardDetailPage } from "../card/card-detail/card-detail.page";
+import { CardFavoritePage } from "../card/card-favorite/card-favorite.page";
+
+const routes: Routes = [
+  {
+    path: "tabs",
+    component: TabsPage,
+    children: [
+      {
+        path: "favorite",
+        outlet: "favorite",
+        component: CardFavoritePage,
+      },
+      {
+        path: "card",
+        outlet: "card",
+        component: CardDeckPage,
+      },
+      {
+        path: "card/:cardId",
+        outlet: "card",
+        component: CardDetailPage,
+      },
+      {
+        path: "card/:cardDeckGroup/:cardDeck",
+        outlet: "card",
+        component: CardListingPage,
+      },
+    ],
+  },
+  {
+    path: "",
+    redirectTo: "/tabs/(card:card)",
+    pathMatch: "full",
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
