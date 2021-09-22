b2f3d5a226833dcf21d16cbafdc1372d59ff0074
bgoonz <bryan.guner@gmail.com>
2021-09-22 01:15:35 +0000

    update vscode extensions

---
+++ _VSCODE-extensions/vscode-markdown-checkboxes/src/index.ts
@@ -0,0 +1,18 @@
+import { workspace } from "vscode";
+import * as taskList from "markdown-it-task-lists";
+import type MarkdownIt from "markdown-it";
+
+export function activate() {
+  return {
+    extendMarkdownIt(md: MarkdownIt) {
+      return md.use(() => {
+        const config = workspace.getConfiguration("markdown-checkboxes");
+        return md.use(taskList, {
+          enabled: config.get("enable"),
+          label: config.get("label"),
+          labelAfter: config.get("labelAfter"),
+        });
+      });
+    },
+  };
+}
