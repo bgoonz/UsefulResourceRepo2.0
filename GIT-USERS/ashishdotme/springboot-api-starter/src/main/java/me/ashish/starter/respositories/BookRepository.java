008a896cf8a3be08b41e7a716fe506e14762b7b3
bgoonz <bryan.guner@gmail.com>
2021-09-22 04:15:37 +0000

    hi

---
+++ GIT-USERS/ashishdotme/springboot-api-starter/src/main/java/me/ashish/starter/respositories/BookRepository.java
@@ -0,0 +1,10 @@
+package me.ashish.starter.respositories;
+
+import me.ashish.starter.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+}
