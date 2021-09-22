ada7b0c6d58c8405b150941af12e5cac4a4a8b6b
bgoonz <bryan.guner@gmail.com>
2021-09-22 01:17:21 +0000

    update vscode extensions

---
+++ _VSCODE-extensions/vscode-simple-icons/README.md
@@ -0,0 +1,17 @@
+# Simple icons
+
+This is a simple icon theme for Visual Studio Code that strives to be simple, taking inspiration from official logos when possible.
+I often also take inspiration from other themes such as [VSCode icons](https://github.com/robertohuertasm/vscode-icons), [Material icon theme](https://github.com/PKief/vscode-extension-material-icon-theme), and [Nomo dark icons](https://github.com/be5invis/vscode-iconset).
+Thanks to Roberto Huertas, Philipp Kief and be5invis for these icon themes !
+
+Since version 1.1.0, a monochrome version of the theme is also available. This idea stems from [file-icons](https://github.com/file-icons/vscode) made by Daniel Brooker.
+
+Since version 1.8.0, the extension can be configured to show or hide rotating triangles/arrows near folders : just change `simpleIcons.hideArrows` to `true` or `false`.
+
+Since version 1.10.0, the standard folder color can be configured with the `simpleIcons.standardFolderColor` setting.
+
+A preview of simple icons can be found in [ICONS.md](ICONS.md).
+
+## Contributing
+
+See CONTRIBUTING.md
---
+++ _VSCODE-extensions/vscode-simple-icons/source/extension.ts
@@ -0,0 +1,129 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as vscode from "vscode";
+
+const colorRegex = /#[0-9A-F]{6}(?![0-9A-F])/gi;
+
+export async function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration(() => probeConfiguration(context))
+  );
+  return await probeConfiguration(context);
+}
+
+export async function deactivate() {}
+
+async function probeConfiguration(
+  context: vscode.ExtensionContext
+): Promise<void> {
+  const somethingChanged =
+    (await toggleArrows(context)) || (await changeFolderColor(context));
+
+  if (somethingChanged) {
+    await vscode.window
+      .showInformationMessage(
+        "The window must be reloaded for changes to take effet",
+        "Reload"
+      )
+      .then((choice) =>
+        choice
+          ? vscode.commands.executeCommand("workbench.action.reloadWindow")
+          : null
+      );
+  }
+}
+
+async function toggleArrows(
+  context: vscode.ExtensionContext
+): Promise<boolean> {
+  const jsonFiles = ["simple", "minimalistic"].map(
+    (name) => name + "-icons.json"
+  );
+  let somethingChanged = false;
+
+  for (const file of jsonFiles) {
+    await changeFile(context, file, async (content) => {
+      let json = JSON.parse(content);
+      const conf = vscode.workspace
+        .getConfiguration("simpleIcons")
+        .get("hideArrows", false);
+
+      if (json.hidesExplorerArrows === conf) {
+        return null;
+      }
+
+      somethingChanged = true;
+      json.hidesExplorerArrows = conf;
+      return JSON.stringify(json, null, 4);
+    });
+  }
+
+  return somethingChanged;
+}
+
+async function changeFolderColor(
+  context: vscode.ExtensionContext
+): Promise<boolean> {
+  let color = vscode.workspace
+    .getConfiguration("simpleIcons")
+    .get<string>("simple.folder.color", null);
+  const oldColor = context.globalState.get<string>("simple.folder.color");
+
+  if (!color) {
+    if (oldColor) {
+      color = oldColor;
+      await context.globalState.update("simple.folder.color", undefined);
+    } else {
+      return false;
+    }
+  } else if (!color.match(colorRegex)) {
+    await vscode.window.showWarningMessage(
+      "Folder color not in hex format, color not changed"
+    );
+    return false;
+  }
+
+  const folders = ["folder", "folder.expanded"].map(
+    (p) => path.join("icons", "simple-icons", p) + ".svg"
+  );
+  let somethingChanged = false;
+
+  for (const folderPath of folders) {
+    await changeFile(context, folderPath, async (content) => {
+      const contentMatches = content.match(colorRegex);
+      const originalColor = contentMatches[0];
+
+      if (originalColor === color) {
+        return null;
+      }
+
+      somethingChanged = true;
+
+      if (color !== oldColor) {
+        await context.globalState.update("simple.folder.color", originalColor);
+      }
+
+      return content.replace(colorRegex, color);
+    });
+  }
+
+  return somethingChanged;
+}
+
+async function changeFile(
+  context: vscode.ExtensionContext,
+  name: string,
+  dg: (content: string) => Promise<string>
+): Promise<void> {
+  const file = context.asAbsolutePath(name);
+  const content = await new Promise<string>((resolve) =>
+    fs.readFile(file, (err, data) => resolve(data.toString()))
+  );
+  const newContent = await dg(content);
+
+  if (newContent) {
+    return await new Promise<void>((resolve) =>
+      fs.writeFile(file, newContent, (err) => resolve())
+    );
+  }
+}
