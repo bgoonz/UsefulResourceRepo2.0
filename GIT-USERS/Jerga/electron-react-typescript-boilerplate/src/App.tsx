be7e3e0fd30d9183f59496f1ec9aafa77f5f2876
bgoonz <bryan.guner@gmail.com>
2021-09-22 04:17:34 +0000

    hi

---
+++ GIT-USERS/Jerga/electron-react-typescript-boilerplate/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>I am App Component!!!</h1>
+      <button
+        onClick={() => {
+          electron.notificationApi.sendNotification("My custom notification!");
+        }}
+      >
+        Notify
+      </button>
+    </>
+  );
+}
---
+++ GIT-USERS/Jerga/electron-react-typescript-boilerplate/webpack.common.js
@@ -0,0 +1,55 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  target: "electron-renderer",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+      {
+        test: [/\.s[ac]ss$/i, /\.css$/i],
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
